29358c1ef15e9ffb79ef2b6dc3d7007bc1d4fc06
ilkrepk <epikilker@gmail.com>
2017-06-04 23:52:59 +0300

    tkelime eklendi

---
+++ getTKelime.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="GET"){
+    include 'connect.php';
+    getKullanici();
+}
+
+function getKullanici()
+{
+    global $connect;
+    if(isset($_GET['istek']))
+    {
+        $request=$_GET['istek'];
+        $query="SELECT * from kelimeler WHERE CKelime='$request'";
+        $result = mysqli_query($connect,$query);
+        $number_of_rows = mysqli_num_rows($result);
+        $temp_array = array();
+
+        if($number_of_rows > 0)
+        {
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $temp_array[]=$row;
+            }
+        }
+        $json_result = array("sayi" => $number_of_rows, "kelimeler"=>$temp_array);
+
+        header ('Content-Type: application/json');
+        echo json_encode ($json_result);
+        mysqli_close($connect);
+    }
+}
+?>
+
