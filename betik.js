428546e485f2cf7abfd5b5e96731d09c3e0ff8e2
ilkrepk <epikilker@gmail.com>
2017-06-12 19:33:50 +0300

    betik.js, insertUyeler.js, sessioninex.php, ve diğer session ile ilgili gerekesiz dosyalar silindi..

--- betik.js
+++
@@ -1,9 +0,0 @@
-window.onload=function () {
-    document.getElementById("basic-addon3").onclick = calis;
-    function calis() {
-        var icerik = document.getElementById("basic-addon3").innerHTML;
-        alert(icerik);
-    }
-}
-
-
